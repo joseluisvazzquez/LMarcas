4a76b2da0d7ee8893e8a51a3b485c641d6a8b752
joseluisvazzquez <jvazquez3@nigul.cide.es>
2023-12-05 17:04:07 +0100

    ACT21

--- Actividad21/eje2.js
+++ Actividad21/eje2.js
@@ -1,11 +1,17 @@
 var readline = require('readline');
 
 var rl = readline.createInterface(process.stdin, process.stdout);
+rl.question("Pasame en una temperatura:", (num) => {
 
-rl.question("Pasame una temperatura:",(num)=>{
-    rl.close()
-})
+    rl.question("Esta en Farenheit(F) o Celsius(C). Escribe (F) o (C):", (temp) => {
+        let c = (num - 32) * (5 / 9)
+        let f = (num * 9 / 5) + 32
+        if (temp == "F") {
 
-rl.question("Esta en Farenheit(F) o Celsius(C). Escribe (F) o (C):",(temp)=>{
-    
-})
+            console.log("Tu temperatura de Farenheit a Celsius es:", c)
+        } else {
+            console.log("Tu temperatura de Celsius a Farenheit es:", f)
+        }
+        rl.close()
+    });
+});
